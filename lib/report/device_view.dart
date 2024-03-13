93cc77b3118057d0dc6bd2fa7abdcbe8fe04aec1
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-03-12 20:07:07 -0700

    Implemented DeviceView

--- lib/report/device_view.dart
+++ lib/report/device_view.dart
@@ -1,18 +1,40 @@
-import 'package:flutter/material.dart';
+import 'package:bluetooth_detector/report/report.dart';
 import 'package:bluetooth_detector/styles/colors.dart';
+import 'package:bluetooth_detector/styles/button_styles.dart';
+import 'package:flutter/material.dart';
 
-class DeviceView extends StatefulWidget {
-  String uuid;
+class DeviceView extends StatelessWidget {
+  Device device;
 
-  DeviceView({super.key, required this.uuid});
+  DeviceView({super.key, required this.device});
 
-  @override
-  DeviceViewState createState() => DeviceViewState();
-}
-
-class DeviceViewState extends State<DeviceView> {
   @override
   Widget build(BuildContext context) {
-    return Text(widget.uuid, style: const TextStyle(color: colors.primaryText));
+    return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+        child: TextButton(
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const Text("Text")));
+            },
+            style: AppButtonStyle.deviceButtonStyle,
+            child: Table(columnWidths: const {
+              0: FlexColumnWidth(1.0),
+              1: FlexColumnWidth(3.0),
+            }, children: [
+              TableRow(children: [
+                const Text("UUID", style: TextStyle(color: colors.primaryText)),
+                Text(device.device.remoteId.toString(), style: const TextStyle(color: colors.primaryText)),
+              ]),
+              TableRow(children: [
+                const Text("Name", style: TextStyle(color: colors.primaryText)),
+                Text(device.device.advName == "" ? "None" : device.device.advName,
+                    style: const TextStyle(color: colors.primaryText)),
+              ]),
+              TableRow(children: [
+                const Text("Platform", style: TextStyle(color: colors.primaryText)),
+                Text(device.device.platformName == "" ? "Unknown" : device.device.platformName,
+                    style: const TextStyle(color: colors.primaryText)),
+              ]),
+            ])));
   }
 }
