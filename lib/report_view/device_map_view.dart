557a7e1d8eccee4217d367c854073d88b3664a13
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-03-13 20:25:47 -0700

    Created DeviceMapView

---
+++ lib/report_view/device_map_view.dart
@@ -0,0 +1,35 @@
+import 'package:bluetooth_detector/map_view/map_view.dart';
+import 'package:bluetooth_detector/report/report.dart';
+import 'package:bluetooth_detector/styles/button_styles.dart';
+import 'package:bluetooth_detector/styles/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+
+class DeviceMapView extends StatefulWidget {
+  final DeviceIdentifier device;
+  final Report report;
+
+  const DeviceMapView({super.key, required this.device, required this.report});
+
+  @override
+  DeviceMapViewState createState() => DeviceMapViewState();
+}
+
+class DeviceMapViewState extends State<DeviceMapView> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      MapView(),
+      BackButton(
+        color: colors.primaryText,
+        onPressed: () => Navigator.pop(context),
+        style: AppButtonStyle.buttonWithBackground,
+      ),
+    ]);
+  }
+}
