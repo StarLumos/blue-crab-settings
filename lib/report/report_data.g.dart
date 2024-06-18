58b83afd57080ce0f6b9cdd6369bc6e03db1532f
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-06-17 19:08:03 -0700

    Fixed JSON

--- lib/report/report_data.g.dart
+++ lib/report/report_data.g.dart
@@ -8,12 +8,9 @@ part of 'report_data.dart';
 
 ReportData _$ReportDataFromJson(Map<String, dynamic> json) => ReportData()
   ..time = DateTime.parse(json['time'] as String)
-  ..data = (json['data'] as List<dynamic>)
-      .map((e) => Datum.fromJson(e as Map<String, dynamic>))
-      .toList();
+  ..data = (json['data'] as List<dynamic>).map((e) => Datum.fromJson(e as Map<String, dynamic>)).toList();
 
-Map<String, dynamic> _$ReportDataToJson(ReportData instance) =>
-    <String, dynamic>{
-      'time': instance.time.toIso8601String(),
-      'data': instance.data,
+Map<String, dynamic> _$ReportDataToJson(ReportData instance) => <String, dynamic>{
+      "\"time\"": "\"${instance.time.toIso8601String()}\"",
+      "\"data\"": instance.data.map((x) => x.toJson()).toList(),
     };
